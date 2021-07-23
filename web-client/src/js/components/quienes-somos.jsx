94a44d372e813350901259a710c91d903638b1d1
amandaribeirinho@outlook.es <amandaribeirinho@outlook.es>
2021-07-23 15:18:17 +0200

    agregado estilo en barra de navegación y quienes somos

--- web-client/src/js/components/quienes-somos.jsx
+++ web-client/src/js/components/quienes-somos.jsx
@@ -1,7 +1,18 @@
 import * as React from "react";
 
- export const QuienesSomos = () => (
+export const QuienesSomos = () => <section>
 
-     <h1>Aqui pondremos quienes somos</h1>
+    <h2> Misión </h2>
+    <p>Nuestra empresa se fundó a raíz de un viaje que nuestra fundadora, Marina Herrán, realizó en bicicleta por Cataluña en el verano de 2001. Durante ese periodo,
+        descubrió que las opciones turísticas que ofrecían ciudades como Barcelona, Girona y Reus dependían de transportes contaminantes y actividades poco respetuosas con la naturaleza y el entorno, algo que causaba un progresivo
+        deterioro de los increíbles parajes naturales de Cataluña.
+        Preocupada por la falta de alternativas eco-friendly, Marina se propuso fundar una agencia de experiencias turísticas donde el respeto por la naturaleza fuera la principal prioridad.
+        Así nació Econotravel Barcelona.</p>
 
- )
+    <h3> Contacto </h3>
+    <h4>Teléfono: 93 000 00 00</h4>
+    <h4> Correo: info@econotravel.com</h4>
+    <h4>Dirección: Carrer dels Viatgers, 28, 08000 Barcelona</h4>
+
+</section>
+
