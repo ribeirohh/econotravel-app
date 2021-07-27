98e72d0af20b838bc67a4cb853e729291d3512f7
Ana Lissette <lissilab@hotmail.com>
2021-07-27 09:49:58 +0200

    arreglando conflicto src

--- web-client/src/js/components/app.jsx
+++ web-client/src/js/components/app.jsx
@@ -2,9 +2,8 @@ import * as React from "react";
 import {NavigationBar} from "./navigationBar";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import {QuienesSomos} from "./quienes-somos";
-import { Experiencias} from "./experiencias";
-import {Inicio} from "./inicio";
-import Search from './search';
+import {Experiencias} from "./experiencias";
+import {Inicio} from "./inicio"
 
 export const App = () => {
 
